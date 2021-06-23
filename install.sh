494b754a949eed950d88de5f1a0e7cd1287b4d1e
PratikBhusal <PratikBhusal@users.noreply.github.com>
2021-06-23 13:49:38 -0500

    Refactor install script
    It now properly creates a backup of existing files before overriding
    them.
    
    Assuming you are using a POSIX compliant operating system, only git is
    required to install (and use/configure!) my dotfiles.
    
    If rsync is available, it will use it as it is still significantly
    faster than the alternative method to moving the files to their correct
    location.

--- install.sh
+++ install.sh
@@ -6,37 +6,81 @@ if ! [ -x "$(command -v git)" ]; then
 fi
 
 if [ -d "$HOME"/.dotfiles ]; then
-    echo "Bare Dotfiles repo already exists. If you want to get a fresh install, please delete it beforehand."
+    echo "Bare Dotfiles repo already exists. If you want to get a fresh install, please move/delete it."
     exit 2
 fi
 
-git clone --bare git@github.com:PratikBhusal/dotfiles.git "$HOME"/.dotfiles
-cd "$HOME" || exit 1
+# Utilize `dotfiles fetch --deepen=<depth>` to increase depth if rebasing
+git clone                                    \
+    --single-branch                          \
+    --depth 1                                \
+    --separate-git-dir="$HOME"/.dotfiles     \
+    git@github.com:PratikBhusal/dotfiles.git \
+    "$HOME"/tmpdotfiles                      \
+&& \rm tmpdotfiles/.git
+
+cd "$HOME" || exit 3
 
 dotfiles() {
     git --git-dir="$HOME"/.dotfiles/ --work-tree="$HOME" "$@"
 }
-while ! dotfiles checkout; do
+
+EXISTING_FILES="$(dotfiles status --porcelain --untracked-files=no \
+    | grep -e '^\s[MT]' \
+    | cut -d ' ' -f 3 \
+)"
+if echo "$EXISTING_FILES" | head -c1 | grep -e '.'; then
     mkdir -p ~/.dotfiles-backup
-    dotfiles checkout 2>&1 | grep -E "^\\s+" | cut -f 2 | \
-        sed 's/\(.*\)\//\1 /' | \
-        awk 'NF!=1 {print $0; next} {print "." , $0}' | \
-        xargs -l sh -c "mkdir -p ~/.dotfiles-backup/$0 && \
-        mv ~/$0/$1 ~/.dotfiles-backup/$0/$1" 1> /dev/null 2>&1
-done
+
+    echo "$EXISTING_FILES"                          \
+    | sed 's/\(.*\)\//\1 /'                         \
+    | awk 'NF!=1 {print $0; next} {print "." , $0}' \
+    | awk 'BEGIN {h=" \"$HOME\"/"}
+      { print "mkdir -p" h ".dotfiles-backup/" $1, "&& cp" h $1 "/" $2 h ".dotfiles-backup/" $1 "/" $2 }' \
+    | sh
+fi
 
 dotfiles config user.email PratikBhusal@users.noreply.github.com
 
-# if  [ -x "$(command -v git)" ] &&
-#     [ -x "$(command -v rsync)" ] &&
-#     [ ! -d "$HOME/.dotfiles" ]; then
-#     git clone --separate-git-dir="$HOME"/.dotfiles git@github.com:PratikBhusal/dotfiles.git tmpdotfiles
+if [ -x "$(command -v rsync)" ]; then
+    # Formatting & Running Shell Script:
+    # ------------------------------------------------------------------------------
+    # $ \
+    # time \
+    #     rsync --recursive --links tmpdotfiles/ "$HOME"/
+    # rsync --recursive --links tmpdotfiles/ "$HOME"/  0.06s user 0.03s system 107% cpu 0.087 total
+    # Total Time: 0.087
+    rsync --recursive --verbose --links tmpdotfiles/ "$HOME"/
+    \rm -rf tmpdotfiles
+else
+    # Make directories then copy files
+    #
+    # $
+    # time
+    #     {
+    #         find ./tmpdotfiles -type d \
+    #         | cut -d '/' -f 3- \
+    #         | awk -F '\n' 'BEGIN {h="$HOME/"} { print "mkdir -p \"" h $1 "\"" }' \
+    #         &&
+    #         find ./tmpdotfiles \( -type f -o -type l \) \
+    #         | cut -d '/' -f 3-         \
+    #         | awk -F '\n' 'BEGIN {h="$HOME/"}
+    #           { print "cp -P \"" h "tmpdotfiles/" $1 "\" \"" h $1 "\"" }'
+    #     } | sh
+    # { find ./tmpdotfiles -type d | cut -d '/' -f 3- | awk -F '\n'  && find  \(  f}  0.01s user 0.00s system 178% cpu 0.007 total
+    # sh  0.19s user 0.09s system 104% cpu 0.270 total
+    # Total Time: 0.277
+    {
+        find ./tmpdotfiles -type d \
+        | cut -d '/' -f 3- \
+        | awk -F '\n' 'BEGIN {h="$HOME/"} { print "mkdir -p \"" h $1 "\"" }' \
+        &&
+        find ./tmpdotfiles \( -type f -o -type l \) \
+        | cut -d '/' -f 3-         \
+        | awk -F '\n' 'BEGIN {h="$HOME/"}
+          { print "cp -P \"" h "tmpdotfiles/" $1 "\" \"" h $1 "\"" }'
+    } | sh
+    \rm -rf tmpdotfiles
+fi
 
-#     rsync --recursive --verbose --links --exclude '.git' tmpdotfiles/ "$HOME"/
-#     rm -r tmpdotfiles
-
-#     dotfiles config --local status.showUntrackedFiles no
-
-# elif [ ! -x "$(command -v git)" ] || [ ! -x "$(command -v rsync)" ]; then
-#     echo "please install git and rsync before trying to install dotfiles"
-# fi
+# dotfiles config --local status.showUntrackedFiles no
