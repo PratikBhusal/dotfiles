ede757ca2c443c6fc76b751ea33d6c295f3af45d
PratikBhusal <PratikBhusal@users.noreply.github.com>
2023-08-28 23:20:03 -0500

    powerlevel10k: Fix nerdfont spacing issues

--- .config/zsh/plugin-configuration/powerlevel10k.zsh
+++ .config/zsh/plugin-configuration/powerlevel10k.zsh
@@ -71,6 +71,12 @@
     newline                   # \n
   )
 
+  # Defines character set used by powerlevel10k. It's best to let `p10k configure` set it for you.
+  typeset -g POWERLEVEL9K_MODE=nerdfont-complete
+  # When set to `moderate`, some icons will have an extra space after them. This is meant to avoid
+  # icon overlap when using non-monospace fonts. When set to `none`, spaces are not added.
+  typeset -g POWERLEVEL9K_ICON_PADDING=moderate
+
   # Basic style options that define the overall prompt look.
   typeset -g POWERLEVEL9K_BACKGROUND=                            # transparent background
   typeset -g POWERLEVEL9K_{LEFT,RIGHT}_{LEFT,RIGHT}_WHITESPACE=  # no surrounding whitespace
