64ce2ea0cb5cda5c8a4e1daaffbb72686d321e97
PratikBhusal <PratikBhusal@users.noreply.github.com>
2023-12-07 17:28:21 -0600

    Fix non-standard function definition

--- .config/sh/functions.d/pacman.sh
+++ .config/sh/functions.d/pacman.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env sh
 
-function pacfindexec() {
+# If not running interactively, don't do anything
+case "$-" in
+    *i*) ;;
+    *)   return ;;
+esac
+
+pacfindexec() {
   for ARG in $(pacman -Qql $1); do
     [ ! -d $ARG ] && [ -x $ARG ] && echo $ARG;
   done
